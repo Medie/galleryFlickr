0a1fa1f4ebb6bd91df01473c5117dce1db583a07
Med <mmedie@gmail.com>
2024-04-15 03:45:32 +0200

    get images from api and loads them and loads more images

--- frontend/script.js
+++ frontend/script.js
@@ -1,44 +1,55 @@
-// I need to call the backend and get data and show it on the frontend.
-// I will use fetch to call the backend.
+const formEl = document.querySelector("form");
+const searchInputEl = document.getElementById("search-input");
+/*
+Here i am using a className instead of an ID so a  dot is needed before the class name */
+const searchResults = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-btn");
 
-const searchInput = document.getElementById('search-input');
-const searchButton = document.getElementById('search-button');
-const searchResults = document.getElementById('search-results');
+let inputData = "";
+let page = 1;
+let perPage = 10;
 
-const APIURL = `http://localhost:3000/search/bird?page=1&perPage=3`;
+const searchImages = async () => {
+  inputData = searchInputEl.value;
+  const url = `http://localhost:3000/search/${inputData}?page=${page}&perPage=${perPage}`;
 
-async function fetchData() {
-  const response = await fetch(APIURL);
-  const respData = await response.json();
-  console.log(respData);
-
-  respData.forEach((photo) => {
-    const img = document.createElement('img');
-    img.src = photo.imageUrl;
-    img.alt = photo.title;
-    //document.searchResults.appendChild(img);
-    document.body.appendChild(img);
-  });
-}
-
-fetchData();
-
-/* searchButton.addEventListener('click', async () => {
-  const searchTerm = searchInput.value;
   const response = await fetch(url);
+  const respData = await response.json();
 
-  const data = await response.json();
+  if (page === 1) {
+    searchResults.innerHTML = "";
+    //showMore.style.display = "none";
+  }
 
-  console.log(data);
+  const results = respData;
 
-  const results = data.photos.photo;
-  searchResults.innerHTML = '';
-  results.forEach((result) => {
-    const img = document.createElement('img');
-    img.src = `https://live.staticflickr.com/${result.server}/${result.id}_${result.secret}_b.jpg`;
+  //results.map((photo) => {
+  results.forEach((photo) => {
+    console.log("photo");
+    const imageWrapper = document.createElement("div");
+    imageWrapper.classList.add("search-result");
+    const image = document.createElement("img");
+    image.src = photo.imageUrl;
+    image.alt = photo.title;
+    const title = document.createElement("a");
+    title.textContent = photo.title;
 
-    searchResults.appendChild(img);
+    imageWrapper.appendChild(image);
+    imageWrapper.appendChild(title);
+    searchResults.appendChild(imageWrapper);
   });
-}); */
 
-// Path: backend/index.js
+  page++;
+  if (page > 1) {
+    showMore.style.display = "block";
+  }
+};
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  page = 1;
+  searchImages();
+});
+showMore.addEventListener("click", () => {
+  searchImages();
+});
