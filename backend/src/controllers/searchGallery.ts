97fe38f6e0522e2ffc567d6637bd9dd6f40ca520
Med <mmedie@gmail.com>
2024-04-18 14:17:33 +0200

    Updated readme and centered content

--- backend/src/controllers/searchGallery.ts
+++ backend/src/controllers/searchGallery.ts
@@ -3,19 +3,16 @@ import { searchService } from '../services/searchService';
 
 
 export  const  searchController = async (req: Request, res: Response, next: NextFunction) =>{
-    
-    console.log(' req.query.page: ', req.query.page, 'req.query.page: ', req.query.perPage, 'req.params.searchTerm:', req.params.searchTerm);
-
+        console.log(' req.query.page: ', req.query.page, 'req.query.page: ', req.query.perPage, 'req.params.searchTerm:', req.params.searchTerm);
     
     const searchTerm = req.params.searchTerm;
     const page = req.query.page || 1;
     const perPage= req.query.perPage || 10;
      
-
     try {
        
         const result = await searchService({ searchTerm,  page: Number(page),  perPage: Number(perPage) });
-        res.send(result);
+         res.send(result);
     }
     catch (err) {
         next(err);
